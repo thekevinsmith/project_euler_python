563f24971d8b28313eb0f02c03cc5051818d4bf0
kevinsmith <kevin@sparkatm.co.za>
2018-07-18 20:41:39 +0200

    FizzBuzz problem completed. 3and5 problem started.

---
+++ fizzbuzz_3and5.py
@@ -0,0 +1,10 @@
+
+for i in range(1, 100):
+    word = []
+    if i % 3 == 0:
+        word.append('fizz')
+    if i % 5 == 0:
+        word.append('buzz')
+    if len(word) == 0:
+        word.append(str(i))
+    print(''.join(word))
