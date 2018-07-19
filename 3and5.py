b4db7a71b2a2d670e48234bd8fc71922fc03f476
kevinsmith <kevin@sparkatm.co.za>
2018-07-19 15:21:13 +0200

    3and5 Multiples solution completed.

--- 3and5.py
+++ 3and5.py
@@ -2,7 +2,13 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
-
+sum = 0
 for i in range(1, 1000):
-    print()
+    if i % 3 == 0:
+        sum = sum + i
+    elif i % 5 == 0:
+        sum = sum + i
+    else:
+        sum = sum
+print(sum)
     # TODO: Continue
