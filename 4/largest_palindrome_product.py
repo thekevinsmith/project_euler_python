f9f9f52945f598ffe25a2957867d0a4416c0b663
kevinsmith <kevin@sparkatm.co.za>
2018-07-23 22:14:28 +0200

    Problem 4 working correctly, but its not dynamic and will only work for 3 digit x 3 digit product with a total digit count of 6.

--- 4/largest_palindrome_product.py
+++ 4/largest_palindrome_product.py
@@ -3,27 +3,9 @@
 # made from the product of two 2-digit numbers is 9009 = 91 × 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-# TODO: ALL
-#  Find palindromic numbers with 3 digits,
 
-# We need to find all 3 digit palindromic numbers.
-# Then we need to multiply them together to find the largest palindrome
-# made from the product of two 3-ds palindromic numbers
-
-
-# 9009
-#
-# 9009/1000   =   9
-# 9009%10     =   9
-#
-# 9009//100   =   0
-# 9009 % 100  =   0.9
-
-# Pobs wrong
 def main():
-    count = 0
     largest = 0
-
     for i in range(0, 1000, 1):
 
         count = 0
@@ -35,8 +17,6 @@ def main():
 
         if count == 3:
             prodNum.append(i)
-            print(prodNum)
-    # found all 3 digit numbers
 
     for p in range(len(prodNum)):
         for n in range(len(prodNum)):
@@ -50,10 +30,12 @@ def main():
                 count += 1
             if count == 6:
                 sixNum.append(result)
-                for c in range(0, count // 2, 1):
-                    if (result // 10 ** (count - 1 - c) % 10) == (result // 10 ** (c) % 10):
-                        if result > largest:
-                            largest = result
+                if (result // 10 ** 5 % 10) == (result // 10 ** 0 % 10):
+                    if (result // 10 ** 4 % 10) == (result // 10 ** 1 % 10):
+                        if (result // 10 ** 3 % 10) == (result // 10 ** 2 % 10):
+                            palindromeNum.append(result)    # all that fit criteria
+                            if result > largest:
+                                largest = result
 
     print(largest)
 
@@ -69,22 +51,26 @@ if __name__ == '__main__':
     palindromeNum = []
     prodNum = []
     sixNum = []
-    #main()
+    main()
+
+
+
+
     # 994009
 
-    largest = 0
-    count = 6
-    result = 994009
-    for c in range(0, count // 2, 1):
-        if (result // 10 ** (count - 1 - c) % 10) == (result // 10 ** (c) % 10):
-            if result > largest:
-                largest = result
-    print(result)
-
-    print(994009// 10**(6-1-0) % 10)# MS
-    print(994009 // 10 ** (0) % 10) # LS
-
-    print(994009 // 10 ** (6 - 1 - 1) % 10)
-    print(994009 // 10 ** (1) % 10)
+    # largest = 0
+    # count = 6
+    # result = 994009
+    # for c in range(0, count // 2, 1):
+    #     if (result // 10 ** (count - 1 - c) % 10) == (result // 10 ** (c) % 10):
+    #         if result > largest:
+    #             largest = result
+    # print(result)
+    #
+    # print(994009// 10**(6-1-0) % 10)# MS
+    # print(994009 // 10 ** (0) % 10) # LS
+    #
+    # print(994009 // 10 ** (6 - 1 - 1) % 10)
+    # print(994009 // 10 ** (1) % 10)
 
 
