e7360db402eb5e40711fb87e880d1f8e5d5f5cce
kevinsmith <kevin@sparkatm.co.za>
2018-07-24 19:45:41 +0200

    Added Problems 8 to 15 and set up the init for each.

---
+++ 15/lattice_paths.py
@@ -0,0 +1,13 @@
+# Problem 15 : Statement :  Lattice paths
+# Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+#  there are exactly 6 routes to the bottom right corner.
+# How many such routes are there through a 20×20 grid?
+# See the web page
+
+
+def main():
+    print()
+
+
+if __name__ == '__main__':
+    main()
