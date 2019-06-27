9f84ae6a2588159ee6803fb391f0ae4ea12f818e
Kevin Smith <kevinsmith.kis@gmail.com>
2019-06-27 19:11:56 +0200

    Problem 15 Completed.
    Not gonna lie, this wasn't easy, especially in python, with freaking listed lists.

--- 15/lattice_paths.py
+++ 15/lattice_paths.py
@@ -6,28 +6,9 @@
 # this is a more difficult algorithmic problem. 6 ways to go down and right.
 
 
-# 1   1   1   1
-# 1   2   3   4
-# 1   3   6   10
-# 1   4   10  20
-
-# a = [[1, 2, 3], [4, 5, 6]]
-# print(a[0])
-# print(a[1])
-#
-# b = a[0]
-# print(b)
-# print(a[0][2])
-# a[0][1] = 7
-# print(a)
-# print(b)
-# b[2] = 9
-# print(a[0])
-# print(b)
-
 def main():
 
-    gs = 2
+    gs = 20
     grid = []
 
     # this sets up the basics
@@ -40,54 +21,14 @@ def main():
                 row.append(0)
         grid.append(row)
 
-    print(grid)
-
-    # again for the heavy stuff
-    for x in range(0, gs+1):
-        row = []
-        for y in range(0, gs+1):
-            if y == 0 or x == 0:
-                row.append(1)
-            else:
-                row.append(0)
-        grid.append(row)
-
-    # for
-    print(grid)
-
-    # array has been initialized.
-
-
-
-
-
-
-    # grid = []
     # print(grid)
 
-
-    # n = 2
-    # grid = []
-    # for i in range(0, n+1):
-    #     for j in range(0, n+1):
-    #         paths = grid[i+1, j] + grid[i, j+1]
-
-
-    # w*h
-    # 0x1 grid => paths available = 1
-    # 1x1 grid => paths available = 2
-    # 2x1 grid => paths available = 3
-    # 2x2 grid => paths available = 6
-    # 3x1 grid => paths available = 4
-
-    # size = 2 # 2x2
-    # grid = [size+1, size+1]
+    # apply the heavy lifting.
+    for i in range(1, gs+1):
+        for j in range(1, gs+1):
+            grid[i][j] = grid[i-1][j] + grid[i][j-1]
     # print(grid)
-
-    # grid[2, 2] = grid[1, 1] + grid[1, 1]
-    # print(grid)
-
-
+    print(grid[gs][gs])
 
 if __name__ == '__main__':
     main()
